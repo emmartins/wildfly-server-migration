b785f93f3a5667b072f75fed5da73cc2f7bcd077
emmartins <emartins@redhat.com>
2019-01-20 04:13:45 +0000

    CMTOOL-237: adds eap 7.3 server module

---
+++ servers/eap7.3/src/main/java/org/jboss/migration/eap/EAPServer7_3.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.migration.eap;
+
+import org.jboss.migration.core.ProductInfo;
+import org.jboss.migration.core.Server;
+import org.jboss.migration.core.env.MigrationEnvironment;
+import org.jboss.migration.wfly10.ServiceLoaderWildFlyServerMigrations10;
+import org.jboss.migration.wfly10.WildFlyServerMigrations10;
+import org.jboss.migration.wfly.WildFly15_0Server;
+
+import java.nio.file.Path;
+import java.util.ServiceLoader;
+
+/**
+ * The JBoss EAP 7.3 {@link Server}
+ * @author emmartins
+ */
+public class EAPServer7_3 extends WildFly15_0Server {
+
+    private static final WildFlyServerMigrations10 SERVER_MIGRATIONS = new ServiceLoaderWildFlyServerMigrations10<>(ServiceLoader.load(EAPServerMigrationProvider7_3.class));
+
+    public EAPServer7_3(String migrationName, ProductInfo productInfo, Path baseDir, MigrationEnvironment migrationEnvironment) {
+        super(migrationName, productInfo, baseDir, migrationEnvironment);
+    }
+
+    @Override
+    protected WildFlyServerMigrations10 getMigrations() {
+        return SERVER_MIGRATIONS;
+    }
+}
